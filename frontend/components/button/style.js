cc977ae96f4ea7677b39ff0ee7a5f405b612badb
Grish <joshigrish058@gmail.com>
2025-01-14 13:39:26 +0545

    done

---
+++ frontend/components/button/style.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const style = () => {
+  return (
+    <View>
+      <Text>style</Text>
+    </View>
+  );
+};
+
+export default style;
