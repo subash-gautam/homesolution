6f8b85c2277510d8b3b087a997a9aa249950acb7
Subash Gauatm <subashgautam977@gmail.com>
2025-05-16 10:26:34 +0545

    single api change

--- frontend/src/utils/temp.api.js
+++ frontend/src/utils/temp.api.js
@@ -1,14 +1,16 @@
 //* Copy code of this file and paste to  /frontend/src/utils/api.js editing IP address to your own.
 
+const backendUrl = "http://192.168.10.87:3000";
+
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.10.87:3000", {
+const socket = io(`${backendUrl}`, {
 	transports: ["websocket"],
 	reconnection: true,
 });
 
 const backend = {
-	backendUrl: "http://192.168.10.87:3000",
+	backendUrl,
 };
 
 export default backend;
